f3f9ac633e19584cebd885f83c3cec221d5d8555
jxv <jxv@hush.com>
2013-05-15 15:54:45 -0700

    <imp> refactored _core_ aspects into diff files;

---
+++ core.h
@@ -0,0 +1,259 @@
+#ifndef SCABS_CORE_H
+#define SCABS_CORE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <assert.h>
+
+#define NOT(t)	assert((t))
+#define RANGE(a,b,c)	assert((a) >= (b) && (a) <= (c))
+#define XRANGE(a,b,c)	assert((a) > (b) && (a) < (c))
+
+
+#define BOARD_X	15
+#define BOARD_Y	15
+#define BOARD_SIZE	BOARD_Y	/* Max among BOARD_X and BOARD_Y */
+#define RACK_SIZE	7
+#define BAG_SIZE	(100+1)	/* needs extra element for size check */
+#define MAX_PLAYER	4
+
+#define VALID_BOARD_X(x)	RANGE(x,0,BOARD_X-1)
+#define VALID_BOARD_Y(y)	RANGE(y,0,BOARD_Y-1)
+#define VALID_BOARD_SIZE(s)	RANGE(s,0,BOARD_SIZE-1)
+
+
+typedef enum
+{
+	FALSE = 0,
+	TRUE = 1
+} bool;
+
+
+typedef enum
+{
+	TILE_NONE = -1,
+	TILE_WILD = 0,
+	TILE_LETTER,
+	TILE_COUNT
+} tile_t;
+
+
+typedef enum
+{
+	SQ_NORMAL = 0,
+	SQ_DBL_LET,
+	SQ_DBL_WRD,
+	SQ_TRP_LET,
+	SQ_TRP_WRD,
+	SQ_COUNT
+} sq_t;
+
+
+typedef enum
+{
+	LETTER_A = 0,
+	LETTER_B,
+	LETTER_C,
+	LETTER_D,
+	LETTER_E,
+	LETTER_F,
+	LETTER_G,
+	LETTER_H,
+	LETTER_I,
+	LETTER_J,
+	LETTER_K,
+	LETTER_L,
+	LETTER_M,
+	LETTER_N,
+	LETTER_O,
+	LETTER_P,
+	LETTER_Q,
+	LETTER_R,
+	LETTER_S,
+	LETTER_T, 
+	LETTER_U,
+	LETTER_V,
+	LETTER_W,
+	LETTER_X,
+	LETTER_Y,
+	LETTER_Z,
+	LETTER_COUNT
+} letter_t;
+
+
+typedef enum
+{
+	MOVE_INVALID = -1,
+	MOVE_PLACE = 0,
+	MOVE_SWAP,
+	MOVE_SKIP,
+	MOVE_COUNT
+} move_t;
+
+
+typedef enum
+{
+	DIR_INVALID = -1,
+	DIR_RIGHT = 0,
+	DIR_DOWN,
+	DIR_COUNT
+} dir_t;
+
+
+typedef enum
+{
+	PATH_INVALID = -1,
+	PATH_DOT = 0,
+	PATH_HORZ,
+	PATH_VERT,
+	PATH_COUNT
+} path_t;
+
+
+typedef enum
+{
+	ACTION_INVALID = -1,
+	ACTION_PLACE = 0,
+	ACTION_SWAP,
+	ACTION_SKIP,
+	ACTION_COUNT
+} action_t;
+
+
+struct tile
+{
+	tile_t type;
+	letter_t letter;
+};
+
+
+struct board
+{
+	struct tile tile[BOARD_Y][BOARD_X];
+	sq_t sq[BOARD_Y][BOARD_X];
+};
+
+
+struct player
+{
+	int score;
+	struct tile tile[RACK_SIZE];
+};
+
+
+struct coor
+{
+	int x;
+	int y;
+};
+
+
+struct place
+{
+	int num;
+	int rack_id[RACK_SIZE];
+	struct coor coor[RACK_SIZE];
+};
+
+
+struct swap
+{
+	int num;
+	int rack_id[RACK_SIZE];
+};
+
+
+struct move
+{
+	move_t type;
+	int player_id;
+	union {
+		struct place place;
+		struct swap swap;
+	} data;
+};
+
+
+struct bag
+{
+	int head;
+	int tail;
+	struct tile tile[BAG_SIZE];
+};
+
+
+struct dir
+{
+	dir_t type;
+	int x;
+	int y;
+	int length;
+	int pos[BOARD_SIZE];
+};
+
+
+struct path
+{
+	path_t type;
+	union {
+		struct {
+			struct dir right;
+			struct dir down; 
+		} dot;
+		struct {
+			struct dir right;
+			struct dir down[BOARD_X];
+		} horz;
+		struct {
+			struct dir right[BOARD_Y];
+			struct dir down;
+		} vert;
+	} data;
+	struct board board;
+};
+
+
+struct action
+{
+	action_t type;
+	int player_id;
+	union {
+		struct {
+			int score;
+			int num;
+			int rack_id[RACK_SIZE];
+			struct path path;
+		} place;
+		struct swap swap;
+		
+	} data;
+};
+
+
+struct dictionary
+{
+	long num;
+	letter_t **word;
+	int *len;
+};
+
+
+struct game
+{
+	int turn;
+	int player_num;
+	struct player player[MAX_PLAYER];
+	struct board board;
+	struct bag bag;
+	struct dictionary dictionary;
+};
+
+
+void mk_action(struct action*, struct game*, struct move*);
+void apply_action(struct game*, struct action*);
+void next_turn(struct game*);
+int cmp_word(letter_t*, int, letter_t*, int);
+
+#endif
+
