4ac1d4f7cc4c3d1ff6dc257c55a9d841bc6164ff
jxv <jxv@hush.com>
2013-08-13 18:06:53 -0700

    <add> opk buid;

---
+++ mkopk.sh
@@ -0,0 +1,12 @@
+rm -r build finite.opk
+make -f Makefile.gcw0 clean
+make -f Makefile.gcw0
+mkdir build
+mkdir build/res
+cp finite build
+cp icon.png build
+cp default.gcw0.desktop build
+cp -r res/*.png build/res
+cp -r res/*.wav build/res
+cp -r res/*.txt build/res
+mksquashfs build finite.opk -all-root -noappend -no-exports -no-xattrs
